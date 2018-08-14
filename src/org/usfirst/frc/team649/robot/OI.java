b01d89e29f8bb24ebd53dfd37e2fade34f28b040
Krisha <kminocha4@gmail.com>
2018-08-13 20:52:08 -0700

    buttons

--- src/org/usfirst/frc/team649/robot/OI.java
+++ src/org/usfirst/frc/team649/robot/OI.java
@@ -17,6 +17,38 @@ public class OI {
 	
 	Joystick stick1 = new Joystick(0);
 
+	public boolean button1() {
+		return stick1.getRawButton(1);
+	}
+	
+	public boolean button2() {
+		return stick1.getRawButton(2);
+	}
+	
+	public boolean button3() {
+		return stick1.getRawButton(3);
+	}
+	
+	public boolean button4() {
+		return stick1.getRawButton(4);
+	}
+	
+	public double axis0() {
+		return stick1.getRawAxis(0);
+	}
+	
+	public double axis1() {
+		return stick1.getRawAxis(1);
+	}
+	
+	public double axis4() {
+		return stick1.getRawAxis(4);
+	}
+	
+	public double axis5() {
+		return stick1.getRawAxis(5);
+	}
+
 	public double POV(int y) {
 		return stick1.getPOV(y);
 	}
